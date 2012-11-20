69cd743b967a57dc189887e4a2ffac343dd7b2ea
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2012-11-20 15:36:02 +0530

    Fixing the project!! (works with Play 2.0.4 now)

--- project/Build.scala
+++ project/Build.scala
@@ -8,7 +8,7 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0-SNAPSHOT"
 
     val appDependencies = Seq(      
-      "se.radley" %% "play-plugins-salat" % "1.1-SNAPSHOT"
+      "se.radley" %% "play-plugins-salat" % "1.1"
     )
 
     val main = PlayProject(appName, appVersion, appDependencies, mainLang = SCALA).settings(
